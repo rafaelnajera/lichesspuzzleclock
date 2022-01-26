4cb1dc8d906ebc1cad800d80682364cea9ec7f8a
Rafael Nájera <rafael.najera@uni-koeln.de>
2022-01-26 13:53:21 +0100

    First commit, version 1.0

---
+++ README.md
@@ -0,0 +1,93 @@
+# lichesspuzzleclock
+A Violentmonkey user script that adds a clock/timer to lichess puzzles. 
+
+### Credits
+
+Based on Hugo Platzer's script "Lichess timed puzzles"
+(https://greasyfork.org/en/scripts/380560-lichess-timed-puzzles)
+
+
+## Installation
+Create a new user script in Violentmonkey and replace its contents with 
+the contents of `lichesspuzzleclock.js`
+
+
+## Usage
+The clock/timer will appear on top of the chess board. 
+There are three timer buttons: B, R and C (standing for Blitz, Rapid and Classic) 
+By default, the timers are set to B = 10sec, R = 30sec and C  = 3min. 
+These can be changed in the "Values" tab for the script in Violentmonkey after
+the script is run for the first time.  The button marked with an infinite sign
+starts a normal, count-up clock.
+
+When using a timer, if the "AF" checkbox is checked, the script will automatically
+fail the current puzzle when time runs out. If not, when time runs out the
+clock display will turn red and will show the time elapsed as a normal clock.
+
+When time has not yet run out, the timer can be cancelled by clicking the infinite
+button. The clock switches to displaying the elapsed time.
+
+If you want one of the timers (or the normal clock) to start automatically with
+each new puzzle, select the desired timer in the "Auto" dropdown menu. The
+selected timer will auto-start in the next puzzle.
+
+## Why Timed Puzzles?
+
+The idea with adding a time constraint to puzzles should **not** be to put yourself more pressure and make you rush through the puzzle
+guessing one move at a time in desperation, but, of course, you can use the timers that 
+way. We all know it's a lot of fun.
+
+One **good** use of a timer, on the other hand, is to help you find out which puzzles you cannot naturally
+solve within a certain period of time, study those, and gradually be able to solve harder 
+puzzles within that time constraint. 
+
+For timers in the 10 to 30sec range this will help you find out, study and 
+internalize tactical patterns since it is virtually impossible to solve a tactical 
+puzzle in such a low time if you don't recognize the tactical pattern or 
+patterns in the puzzle very quickly without calculation.  The 10sec or less timer 
+will probably force you to work on your mouse dexterity too!
+
+Simply solve each puzzle normally, trying to see the whole solution before making 
+your moves. If you could not solve it, study the solution, go over it 2-3 times, 
+visualize it in your head 2-3 more times and move on to the next. It is said
+that finding and studying 3 of these failed puzzles per day will slowly build your
+arsenal of tactical patterns very efficiently along a 1-2 year period. A 30sec timer is 
+recommended of this purpose.
+
+Longer timers will reveal tactical weaknesses due to lack of tactical patterns 
+and problems with visualization and calculation. They will give you an idea of the 
+kind of tactical problems you might be able to solve in longer time controls and compel
+you to be more efficient in your solving. People with chronic time pressure problems in 
+longer games may benefit a lot from this. 
+
+## Why 10s, 30s and 3 min? 
+
+These values are derived from a calculation of the average time available per move in 
+blitz games, rapid games and (FIDE) classical games, with some liberties so that they 
+have nicely rounded values. There is no super sophisticated or transcendental reasoning 
+behind it.
+
+Most chess games are over in less than 45 moves, so we can calculate time available on 
+a 45 move basis. For a 15+10 rapid game, for example, you have 15' + 45x10" = 22'30", 
+that's 30" per move, which the default Rapid timer.  Normally you will play your opening 
+prep moves much faster, so in reality in a game you have a bit more time per move. 
+For example, if you can play 9 opening moves very fast, you will have about 36" per move 
+for the rest of a 45 move game. Also, of course, some games  will end much sooner, and 
+some might go into an endgame (in which you might have only your 10" increment 
+available). This is not an exact science, so feel free to spend your time bashing these
+calculations online and/or tweaking your timers ad nauseam.
+
+In any case, the same calculation yields 9" per move for a 5+3 blitz game. So 10" is a nice
+round number for the Blitz timer. For a 3+2 game you have about 6" per move. 
+
+For the classical timer, the basis is a 90+30 game, but in this case it is assumed that you
+will play your first 8-9 moves faster. Without that assumption the time available per move 
+should be 2'30".  
+
+There is, of course, no reason to attach any particular meaning to the B, R and C buttons.
+You might decide, for example, to have a timer for the "Easiest" setting, another one for "Normal", and
+another for "Hard". 
+
+
+
+
---
+++ lichesspuzzleclock.js
@@ -0,0 +1,436 @@
+// ==UserScript==
+// @name     LichessPuzzleClock
+// @version  1.0
+// @match https://lichess.org/training*
+// @exclude-match https://lichess.org/training/history
+// @exclude-match https://lichess.org/training/themes
+// @exclude-match https://lichess.org/training/dashboard*
+// @exclude-match https://lichess.org/training/of-player
+// @require https://code.jquery.com/jquery-3.6.0.min.js
+// @inject-into content
+// @grant GM_addStyle
+// @grant GM_getValue
+// @grant GM_setValue
+// ==/UserScript==
+
+
+// User Settings
+
+const timeOutBlitz = getUserSetting('timeOutBlitz', 10)
+const timeOutRapid = getUserSetting('timeOutRapid', 30)
+const timeOutClassic = getUserSetting('timeOutClassic', 180)
+
+
+
+let autoStartMode = getUserSetting('autoStartMode', 'no')
+let autoFailOnTimeout = getUserSetting('autoFailOnTimeOut', false)
+let autoStart = autoStartMode !== 'no'
+
+
+
+// -----------------------------------------------
+// Script body
+
+const STATE_WAITING = 0
+const STATE_COUNTING_UP = 1
+const STATE_COUNTING_DOWN = 2
+const STATE_PUZZLE_TIMEOUT = 3
+const STATE_PUZZLE_END = 4
+
+const classCountingUp = 'chrono'
+const classCountingDown = 'timer'
+const classTimeout = 'timeout'
+const classPuzzleEnd = 'end'
+
+const pollingInterval = 30
+
+// global variables
+let startingTimestamp = -1
+let timeout
+let state
+
+let buttonBlitz
+let buttonRapid
+let buttonClassic
+let buttonInfinite
+let clockElement
+let autoFailCheckbox
+let autoSelect
+
+GM_addStyle(getStyles())
+
+$( () => {
+    $('#top').after(getHtml())
+    buttonBlitz = $(`div.puzzleClock button.blitz`)
+    buttonRapid = $(`div.puzzleClock button.rapid`)
+    buttonClassic = $(`div.puzzleClock button.classic`)
+    buttonInfinite =$(`div.puzzleClock button.infinite`)
+    clockElement = $('div.clock')
+    autoFailCheckbox = $(`div.puzzleClock input.autoFailCheckbox`)
+    autoSelect = $(`div.puzzleClock select.autoSelect`)
+
+    buttonBlitz.on('click', genOnClickTimerButton(timeOutBlitz))
+    buttonRapid.on('click', genOnClickTimerButton(timeOutRapid))
+    buttonClassic.on('click', genOnClickTimerButton(timeOutClassic))
+    buttonInfinite.on('click', genOnClickInfiniteButton())
+
+    autoFailCheckbox.on('click', genOnClickAutoFailCheckbox())
+    autoSelect.on('change', genOnChangeAutoSelect())
+
+   goToInitialState()
+   setInterval(managerThread, pollingInterval)
+
+})
+
+// -----------------------------------------------
+
+
+/**
+ * Gets a user setting from storage, sets it in storage if it's not already there
+ * @param {string}setting
+ * @param {any}defaultValue
+ */
+function getUserSetting(setting, defaultValue) {
+    let v = GM_getValue(setting, null)
+    if (v === null) {
+        GM_setValue(setting, defaultValue)
+        return defaultValue
+    }
+    return v
+}
+
+function getTimeOutButtons() {
+    return [ buttonBlitz, buttonRapid, buttonClassic]
+}
+
+function genOnClickTimerButton(timeOut) {
+    return () => {
+        if (state !== STATE_WAITING) {
+            return
+        }
+        toState(STATE_COUNTING_DOWN, timeOut)
+    }
+}
+
+function genOnClickInfiniteButton() {
+    return ()  => {
+        switch (state) {
+            case STATE_WAITING:
+            case STATE_COUNTING_DOWN:
+                toState(STATE_COUNTING_UP)
+                break
+        }
+    }
+}
+
+function genOnClickAutoFailCheckbox() {
+    return () =>  {
+        autoFailOnTimeout = autoFailCheckbox.is(':checked')
+        GM_setValue('autoFailOnTimeOut', autoFailOnTimeout)
+    }
+}
+
+function genOnChangeAutoSelect() {
+    return () => {
+        let selectedOption = autoSelect.find(':selected').val()
+        GM_setValue('autoStartMode', selectedOption)
+        autoStartMode = selectedOption
+    }
+}
+
+function goToInitialState() {
+    if (autoStart) {
+        let initialTimer = -1
+        switch(autoStartMode) {
+            case 'blitz':
+                initialTimer = timeOutBlitz
+                break
+
+            case 'rapid':
+                initialTimer = timeOutRapid
+                break
+
+            case 'classic':
+                initialTimer = timeOutClassic
+                break
+        }
+        if (initialTimer !== -1) {
+            toState(STATE_COUNTING_DOWN, initialTimer)
+        } else {
+            startingTimestamp = -1
+            toState(STATE_COUNTING_UP)
+        }
+    } else {
+        toState(STATE_WAITING)
+    }
+}
+
+function toState(newState, param = -1) {
+    //console.log(`Going from state ${state} to ${newState}, param = ${param}`)
+    switch(newState) {
+        case STATE_WAITING:
+            clockElement.html(getClockHtml(STATE_WAITING, 0))
+                .removeClass([classCountingUp, classCountingDown, classTimeout, classPuzzleEnd])
+            timeout = -1
+            startingTimestamp = -1
+            getTimeOutButtons().forEach( (btn) => {
+                btn.prop('disabled', false)
+            })
+            buttonInfinite.prop('disabled', false)
+            state = STATE_WAITING
+            break
+
+        case STATE_COUNTING_UP:
+            timeout = -1
+            if (startingTimestamp === -1) {
+                startingTimestamp = new Date()
+            }
+            clockElement.removeClass([classCountingDown, classTimeout, classPuzzleEnd]).addClass(classCountingUp)
+            getTimeOutButtons().forEach( (btn) => {
+                btn.prop('disabled', true)
+            })
+            state = STATE_COUNTING_UP
+            break
+
+        case STATE_COUNTING_DOWN:
+            timeout = param*1000
+            startingTimestamp = new Date()
+            clockElement.removeClass([classCountingUp, classTimeout, classPuzzleEnd]).addClass(classCountingDown)
+            getTimeOutButtons().forEach( (btn) => {
+                btn.prop('disabled', true)
+            })
+            buttonInfinite.prop('disabled', false)
+            state = STATE_COUNTING_DOWN
+            break
+
+        case STATE_PUZZLE_TIMEOUT:
+            clockElement.removeClass(classCountingDown).addClass(classTimeout)
+            buttonInfinite.prop('disabled', true)
+            if (autoFailOnTimeout) {
+                makePuzzleFail()
+            }
+            state = STATE_PUZZLE_TIMEOUT
+            break
+
+        case STATE_PUZZLE_END:
+            clockElement.removeClass([classCountingUp, classCountingDown, classTimeout])
+                .addClass(classPuzzleEnd)
+
+            buttonInfinite.prop('disabled', true)
+            state = STATE_PUZZLE_END
+            break
+    }
+
+}
+
+function  getClockHtml(state, timeInMilliseconds) {
+    let withDecimals = state === STATE_COUNTING_DOWN && timeInMilliseconds < 10000
+    let timeHtml = getTimeHtml(timeInMilliseconds, withDecimals)
+    switch (state) {
+        case STATE_WAITING:
+            return '--:--'
+
+        case STATE_COUNTING_DOWN:
+            return `&raquo; ${timeHtml}`
+
+        case STATE_COUNTING_UP:
+            return `+ ${timeHtml}`
+
+        case STATE_PUZZLE_TIMEOUT:
+            return `&otimes; ${timeHtml}`
+
+        case STATE_PUZZLE_END:
+            return `&oplus; ${timeHtml}`
+    }
+
+
+}
+
+function managerThread() {
+    let currentTimestamp
+    let timeInClock
+    switch(state) {
+        case STATE_WAITING:
+            // nothing to do
+            break
+
+        case STATE_COUNTING_DOWN:
+            if (!isSolvingPuzzle() || isPuzzleWrong()) {
+                // puzzle ended
+                toState(STATE_PUZZLE_END)
+                break
+            }
+            currentTimestamp = new Date()
+            timeInClock = timeout - (currentTimestamp - startingTimestamp)
+            if (timeInClock < 0) {
+                timeInClock = currentTimestamp - startingTimestamp
+                toState(STATE_PUZZLE_TIMEOUT)
+                clockElement.html(getClockHtml(STATE_PUZZLE_TIMEOUT, timeInClock))
+            } else {
+                clockElement.html(getClockHtml(STATE_COUNTING_DOWN, timeInClock))
+            }
+            break
+
+        case STATE_COUNTING_UP:
+            currentTimestamp = new Date()
+            timeInClock = currentTimestamp - startingTimestamp
+            clockElement.html(getClockHtml(STATE_COUNTING_UP, timeInClock))
+            if (!isSolvingPuzzle() || isPuzzleWrong()) {
+                // puzzle ended
+                toState(STATE_PUZZLE_END, timeInClock)
+                break
+            }
+            break
+
+        case STATE_PUZZLE_TIMEOUT:
+            currentTimestamp = new Date()
+            timeInClock = currentTimestamp - startingTimestamp
+            clockElement.html(getClockHtml(STATE_PUZZLE_TIMEOUT, timeInClock))
+            if (!isSolvingPuzzle() || isPuzzleWrong()) {
+                // puzzle ended
+                toState(STATE_PUZZLE_END, timeInClock)
+                break
+            }
+            break
+
+        case STATE_PUZZLE_END:
+            if (!isPuzzleWrong() && isSolvingPuzzle()) {
+                goToInitialState()
+            }
+            break
+    }
+}
+
+
+function getTimeHtml(timeInMilliseconds, withDecimals) {
+    let timeMinutes = Math.floor(timeInMilliseconds / 60000)
+    let timeSeconds = Math.floor((timeInMilliseconds % 60000) / 1000)
+    let timeMillis = timeInMilliseconds % 1000
+    let timeSecondsStr;
+    if (timeSeconds < 10) {
+        timeSecondsStr = "0" + timeSeconds;
+    } else {
+        timeSecondsStr = "" + timeSeconds;
+    }
+    let decString = withDecimals ? `<small>${Math.floor(timeMillis / 100)}</small>` : ''
+
+    return `${timeMinutes}:${timeSecondsStr}${decString}`
+}
+
+function isSolvingPuzzle() {
+    let element = document.querySelector('div.view_solution');
+    return (element !== null);
+}
+
+function isPuzzleWrong() {
+    let element = document.querySelector('div.icon');
+    return (element !== null && element.innerHTML === "✗");
+}
+
+function makePuzzleFail() {
+    let button = document.querySelector('div.view_solution a');
+    if (button !== null) {
+        button.click();
+    }
+}
+
+function getHtml() {
+    return `<div class="puzzleClock">
+        <div class="tb">
+            <div class="tb-group">
+                <button class="blitz" title="Blitz mode, ${timeOutBlitz}s">B</button>
+                <button class="rapid" title="Rapid mode, ${timeOutRapid}s">R</button> 
+                <button class="classic" title="Classic mode, ${timeOutClassic}s">C</button>
+            </div>
+            <div class="tb-group">
+                <button class="infinite" title="No time limit">&infin;</button>
+            </div>
+           
+        </div>
+        <div class="clock">--:--</div>
+        <div class="tb">
+            <div class="tb-group">
+                 <input title="Auto Fail Puzzle on Timeout" type="checkbox" ${autoFailOnTimeout ? "checked" : ''} class="autoFailCheckbox"/> <label>AF</label>
+            </div>
+            <div class="tb-group">
+                <label>Auto:</label>
+                <select title="Auto Start" class="autoSelect" >
+                   <option title="Do no start automatically" value="no" ${autoStartMode === 'no' ? 'selected' : ''}>no</option>
+                   <option title="Auto start Blitz timer" value="blitz" ${autoStartMode === 'blitz' ? 'selected' : ''}>B</option>
+                   <option title="Auto start Rapid timer" value="rapid" ${autoStartMode === 'rapid' ? 'selected' : ''}>R</option>
+                   <option title="Auto start Classic timer" value="classic" ${autoStartMode === 'classic' ? 'selected' : ''}>C</option>
+                   <option title="Auto start normal clock" value="infinite" ${autoStartMode === 'infinite' ? 'selected' : ''}>&infin;</option>
+                </select>
+            </div>
+        </div>
+     </div>`
+}
+
+function getStyles() {
+    return `
+    .pcHidden {
+        display: none;
+    }
+    
+div.puzzleClock {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: 100%; 
+    font-size:12pt;
+}
+
+.puzzleClock div.tb {
+    display: flex;
+    flex-direction: row
+}
+
+.puzzleClock div.tb-group {
+    margin-right: 2em;
+}
+
+.puzzleClock button {
+    color: rgb(186, 186, 186);
+    border: 1px solid rgb(186, 186, 186);
+    border-radius: 3px;
+    margin: 5px;
+    background: none;
+    padding: 1px 8px;
+}
+
+.puzzleClock select {
+    padding:0;
+}
+
+.puzzleClock button:disabled {
+    color: rgb(96, 96, 96);
+    border: 1px solid rgb(96, 96, 96);
+}
+
+
+.puzzleClock div.clock {
+    font-size: 1.5em;
+    margin-left: 3em;
+    margin-right: 3em;
+}
+
+.puzzleClock div.clock.${classCountingDown} {
+    color: yellow;
+}
+
+.puzzleClock div.clock.${classCountingUp} {
+    color: silver;
+}
+
+.puzzleClock div.clock.${classPuzzleEnd} {
+    color: gray;
+}
+
+.puzzleClock div.clock.${classTimeout} {
+    color: red
+}
+
+.puzzleClock div.clock small {
+    font-size: 0.5em;
+}`
+}
